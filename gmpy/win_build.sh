05e836ddca0dcace3468a6f9f7c647a43fc5e891
Jonas Weinz <s6jowein@uni-bonn.de>
2018-12-30 19:27:25 +0100

    fixed gmpy install routine

--- gmpy/win_build.sh
+++ gmpy/win_build.sh
@@ -2,4 +2,11 @@
 
 . $UNIX_STYLE_RECIPE_DIR/environment.sh
 cd $UNIX_STYLE_SRC_DIR
-python ./setup.py build --compiler=mingw32 install
+python ./setup.py build --compiler=mingw32
+
+# for some strange reason the generated gmpy2.h and gmpy2.pxd only contains a relative path to the source pxd in the temporary build folder. So we have to copy th file manually to the generated build
+cp src/gmpy2.pxd build/lib.*/gmpy2/
+cp src/gmpy2.h build/lib.*/gmpy2/ 
+
+python ./setup.py install
+
